c9ccf9e9d01ff3a65c5f513d3b367b08633fceb6
roselandroche <r.landroche@gmail.com>
2020-06-10 21:44:08 -0400

    Moving into items

---
+++ src/item.py
@@ -0,0 +1,7 @@
+# Implement a class to hold item information. This should have name and
+# description attributes. This is a base class.
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
--- src/player.py
+++ src/player.py
@@ -5,3 +5,5 @@ class Player:
     def __init__(self, name, current_room=None):
         self.name = name
         self.current_room = current_room
+
+        self.inventory = []
