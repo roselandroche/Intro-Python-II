0f2f48827127a50b644bfbbceccf376cdfd056d5
roselandroche <r.landroche@gmail.com>
2020-06-10 21:35:40 -0400

    Edits to adv and player

--- src/adv.py
+++ src/adv.py
@@ -2,44 +2,40 @@ from room import Room
 from player import Player
 
 # Declare all the rooms
+outside = Room("Outside Cave Entrance",
+                "North of you, the cave mount beckons")
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+foyer = Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east.""")
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+overlook =  Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""")
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+narrow = Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""")
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+treasure = Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
+earlier adventurers. The only exit is to the south.""")
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+outside.n_to = foyer
+foyer.s_to = outside
+foyer.n_to = overlook
+foyer.e_to = narrow
+overlook.s_to = foyer
+narrow.w_to = foyer
+narrow.n_to = treasure
+treasure.s_to = narrow
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player_one = Player('Rose', room['outside'])
+player_one = Player('Rose', outside)
 
 # Write a loop that:
 while True:
@@ -54,25 +50,22 @@ while True:
     choice = input("Where do you want to go? (n, e, s, w) : ")
 
     # If the user enters a cardinal direction, attempt to move to the room there.
-    try:
-        if choice == 'q':
-            break
+    if choice == 'q':
+        break
+    
+    elif choice == 'n' and player_one.current_room.n_to:
+        player_one.current_room = player_one.current_room.n_to
+
+    elif choice == 'e' and player_one.current_room.e_to:
+        player_one.current_room = player_one.current_room.e_to
+
+    elif choice == 's' and player_one.current_room.s_to:
+        player_one.current_room = player_one.current_room.s_to
+
+    elif choice == 'w' and player_one.current_room.w_to:
+        player_one.current_room = player_one.current_room.w_to
         
-        if choice == 'n' and player_one.current_room.n_to:
-            player_one.current_room = player_one.current_room.n_to
-
-        elif choice == 'e' and player_one.current_room.e_to:
-            player_one.current_room = player_one.current_room.e_to
-
-        elif choice == 's' and player_one.current_room.s_to:
-            player_one.current_room = player_one.current_room.s_to
-
-        elif choice == 'w' and player_one.current_room.w_to:
-            player_one.current_room = player_one.current_room.w_to
-            
-    # Print an error message if the movement isn't allowed.
-        else:
-            print("Whoops! You can't go that way. Try another direction")
-    # If the user enters "q", quit the game.
-    except:
-        print("Invalid input. Please try again.")
+# Print an error message if the movement isn't allowed.
+    else:
+        print("Whoops! You can't go that way. Try another direction")
+# If the user enters "q", quit the game.
